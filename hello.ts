4064a98fc4a0c04444300c4e438332adb2582312
BitlinkAR <aaron@bitlink.com.au>
2020-08-17 10:24:34 +1000

    Hopefully this works?

--- hello.ts
+++ hello.ts
@@ -6,12 +6,24 @@
  */
 
 //% color="#AA278D" weight=100
-namespace hello {
+namespace bitlink {
     
     //%block
     export function helloWorld() {
         radio.sendString("Hello")
         pause(10000)
+    }
+
+    //%block
+    export function addIdToArrays(receivedName: string, nameArray: string[], numberOfPings: number[]) {
+        for (var _i = 0; _i < nameArray.length; _i++){
+            if (receivedName == nameArray[_i]) {
+                numberOfPings[_i]++;
+                return
+            }
+        }
+        nameArray.push(receivedName)
+        numberOfPings.push(1)
     }
 
     // note that Caml casing yields lower case
