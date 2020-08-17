5bc51f7b18080f3f0575a011d35b27470d8f30e4
BitlinkAR <aaron@bitlink.com.au>
2020-08-17 16:03:45 +1000

    dht???

--- hello.ts
+++ hello.ts
@@ -6,7 +6,7 @@
  */
 
 //% color="#AA278D" weight=100
-//% groups=['Project 3.03', 'Data']
+//% groups=['Project 3.03', 'Data', 'Components']
 namespace bitlink {
 
     //% block
@@ -46,5 +46,61 @@ namespace bitlink {
     //% group="Data"
     export function pushValue(value: string, array: string[]) {
         array.push(value);
+    } 
+
+    let _DHTtemperature: number = 0.0
+    let _DHThumidity: number = 0.0
+    let _DHTreadSuccessful: boolean = false
+
+    //% block
+    //% group="Components"
+    export function QueryDHT11(pin: DigitalPin) {
+        let startTime: number = 0
+        let endTime: number = 0
+        let checksum: number = 0
+        let checksumTmp: number = 0
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+        for (let index = 0; index < 40; index++)
+            dataArray.push(false)
+        for (let index = 0; index < 5; index++)
+            resultArray.push(0)
+
+        pins.digitalWritePin(pin, 0) //begin protocol
+        basic.pause(18)
+        pins.setPull(pin, PinPullMode.PullUp) //pull up data pin if needed
+        pins.digitalReadPin(pin)
+        control.waitMicros(20)
+        while (pins.digitalReadPin(pin) == 1);
+        while (pins.digitalReadPin(pin) == 0); //sensor response
+        while (pins.digitalReadPin(pin) == 1); //sensor response
+
+        //read data (5 bytes)
+        for (let index = 0; index < 40; index++) {
+            while (pins.digitalReadPin(pin) == 1);
+            while (pins.digitalReadPin(pin) == 0);
+            control.waitMicros(28)
+            //if sensor pull up data pin for more than 28 us it means 1, otherwise 0
+            if (pins.digitalReadPin(pin) == 1) dataArray[index] = true
+        }
+
+        //convert byte number array to integer
+        for (let index = 0; index < 5; index++)
+            for (let index2 = 0; index2 < 8; index2++)
+                if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+        //verify checksum
+        checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+        checksum = resultArray[4]
+        if (checksumTmp >= 512) checksumTmp -= 512
+        if (checksumTmp >= 256) checksumTmp -= 256
+        if (checksum == checksumTmp) _DHTreadSuccessful = true
+
+        //read data if checksum ok
+        if (_DHTreadSuccessful) {
+                _DHThumidity = resultArray[0] + resultArray[1] / 100
+                _DHTtemperature = resultArray[2] + resultArray[3] / 10;
+        }
+        basic.pause(2000)
     } 
 }
