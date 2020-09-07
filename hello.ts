ec8b686ffc45bda2078881451727e1fa28aae675
Bitlink-Aaron <69446845+Bitlink-Aaron@users.noreply.github.com>
2020-09-07 16:46:33 +1000

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -6,14 +6,15 @@
  * generated for an **exported** function.
  */
 
-//% color="#03a5fc" weight=100
-//% groups=['Project 3.03', 'Data', 'Components']
-namespace bitlink {
-
-    //% weight=45
-    //% group="Project 3.03"
-    //% block="add name $receivedName|to $nameArray|counting contacts with $numberOfPings"
+//% color="#6392ff" weight=99
+//% groups=['Project 3.03 Input', 'Project 3.03 Output']
+namespace bitkitProjects{
+    //% weight=52
+    //% group="Project 3.03 Input" blockExternalInputs=true
+    //% block="add name %receivedName to %nameArray counting contacts with %numberOfPings"
     export function addIdToArrays(receivedName: string, nameArray: string[], numberOfPings: number[]): void {
+        if (nameArray == null || numberOfPings == null)
+            return;
         let index =  nameArray.indexOf(receivedName)
         if (index >= 0)
             numberOfPings[index]++;
@@ -23,10 +24,10 @@ namespace bitlink {
         }
     }
 
-    //% weight=45
-    //% group="Project 3.03"
-    //% block="add name $receivedName to $nameArray"
-    export function addIdToArray(receivedName: string, nameArray: string[]) {
+    //% weight=60
+    //% group="Project 3.03 Input" blockExternalInputs=true
+    //% block="add name $receivedName to array $nameArray"
+    export function addIdToArray(receivedName: string, nameArray: string[]) : void {
         if (nameArray == null)
             return;
         if (nameArray.indexOf(receivedName) == -1){
@@ -35,9 +36,13 @@ namespace bitlink {
         return;
     }
 
-    //% group="Project 3.03"
+    //%weight=55
+    //% group="Project 3.03 Input" blockExternalInputs=true
     //% block="add ID $receivedNum to array $numArray counting contacts with $numPings"
     export function addSerialNumberToArrays(receivedNum: number, numArray: number[], numPings: number[]) : void {
+        if (numArray == null || numPings == null)
+            return;
+        
         let index =  numArray.indexOf(receivedNum)
         if (index >= 0)
             numPings[index]++;
@@ -49,9 +54,12 @@ namespace bitlink {
         return;
     }
 
-    //% group="Project 3.03"
+    //% weight=65
+    //% group="Project 3.03 Input" blockExternalInputs=true
     //% block="add ID $receivedNumber to array $numberArray"
     export function addSerialNumberToArray(receivedNumber: number, numberArray: number[]) : void {
+        if (numberArray == null)
+            return;
         if (numberArray.indexOf(receivedNumber) == -1){
             numberArray.push(receivedNumber)
         }
@@ -60,31 +68,49 @@ namespace bitlink {
 
     
 
-    //% block
-    //% group="Project 3.03"
+    //% block="scroll names from %nameArray with contacts from %numberOfPings"
+    //% group="Project 3.03 Output" blockExternalInputs=true 
+    //% weight=45
     export function printNamesAndPings(nameArray: string[], numberOfPings: number[]){
+        if (nameArray == null || numberOfPings == null)
+            return;
+        
         for (let _i = 0; _i < nameArray.length; _i++){
             basic.showString(nameArray[_i])
             basic.showNumber(numberOfPings[_i])
         }
     }
 
-    //% block
-    //% group="Project 3.03"
+    //% block="scroll IDs from %serialArray with contacts from %numberOfPings"
+    //% group="Project 3.03 Output" blockExternalInputs=true 
+    //% weight=46
     export function printSerialAndPings(serialArray: number[], numberOfPings: number[]){
+        if (serialArray == null || numberOfPings == null)
+            return;
+
         for (let _i = 0; _i < serialArray.length; _i++){
             basic.showNumber(serialArray[_i])
             basic.showNumber(numberOfPings[_i])
         }
     }
 
-    //% block
-    //% group="Project 3.03"
+    //% block="scroll names from %nameArray"
+    //% group="Project 3.03 Output" 
+    //% weight=48
     export function printNames(nameArray: string[]){
+        if (nameArray == null)
+            return;
         for (let _i = 0; _i < nameArray.length; _i++){
             basic.showString(nameArray[_i])
         }
     }
+}
+
+//% color="#03a5fc" weight=100
+//% groups=['Project 3.03 Input', 'Project 3.03 Output', 'Data', 'DHT11']
+namespace bitlink {
+
+    
 
     //% block
     //% group="Data"
@@ -99,8 +125,8 @@ namespace bitlink {
     
     //Copyright (c) 2019 Alan Wang
     //https://github.com/alankrantas/pxt-DHT11_DHT22
-    //% block
-    //% group="Components"
+    //% block="Query DHT11 on pin %pin"
+    //% group="DHT11"
     export function queryDht11(pin: DigitalPin) {
         let checksum: number = 0
         let checksumTmp: number = 0
@@ -149,16 +175,16 @@ namespace bitlink {
         basic.pause(2000)
     } 
 
-    //% block
-    //% group="Components"
+    //% block="current temperature"
+    //% group="DHT11"
     export function dht11Temperature(): number{
         if (_DHTreadSuccessful)
             return _DHTtemperature
         return -90
     }
     
-    //% block
-    //% group="Components"
+    //% block="current humidity"
+    //% group="DHT11"
     export function dht11Humidity(): number{
         if (_DHTreadSuccessful)
             return _DHThumidity
